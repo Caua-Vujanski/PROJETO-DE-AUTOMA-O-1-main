15fb6c901a8d24d9b237dffdb431a9ee0b170baa
Caua Vujanski <cauavujanski4@gmail.com>
2025-11-28 09:26:22 -0300

    Conclusao Unidade de Ensino

--- cypress/e2e/cadastro_Unidades_de_Ensino.cy.js
+++ cypress/e2e/cadastro_Unidades_de_Ensino.cy.js
@@ -44,11 +44,28 @@ cy.visit('https://dev.erp.inovacarreira.com.br/v2/configuracoes/sistema/geral/un
 
     cy.get('input[placeholder="email@email.com"]').type('automatizado@gmail.com')
 
-    .contains('Escolha o Cargo').click()
+    cy.contains('Escolha o Cargo').click()
         cy.contains('div', 'Gestor de Filial').click()
+    
+    cy.contains('Nome').scrollIntoView().parent()
+        .find('input, texarea').clear().type('TESTE AUTOMATIZADO')
 
+    cy.contains('button', 'Avançar').click()
+    cy.contains('button', 'Concluir').click()
+    cy.contains('button', 'Editar Dados').click()
+    cy.contains('button', 'Avançar').click()
 
+    cy.contains('CEP').scrollIntoView().parent()
+        .find('input, textarea').clear().type('87050-730')
+        cy.wait(2000)
 
+    cy.contains('button', 'Avançar').click()
+
+    cy.contains('button', 'Avançar').click()
+    cy.contains('button', 'Concluir').click()
+    cy.contains('button', 'Excluir Dados').click()
+    cy.wait(600)
+    cy.get('[role="dialog"]:visible').contains('button', 'Excluir').click(); 
 
 
 
